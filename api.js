e21381a8b7145f7a51eddfc0ef163ce05791acd9
NurseJoyDR <fico.zuzic@gmail.com>
2025-06-07 14:38:08 +0200

    testiranje rjesavanja iznimke HTTP 404 za dogadaje

--- api.js
+++ api.js
@@ -14,9 +14,9 @@ app.use(cors());
 
 // Rate limiter
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000,
-    max: 100,
-    message: 'Previše zahtjeva, pokušajte kasnije.'
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: 'Previše zahtjeva, pokušajte kasnije.'
 });
 app.use('/api/', limiter);
 
@@ -28,12 +28,12 @@ const client = new MongoClient(uri);
 let db; // dopušta korištenje db u cijelom api-u
 
 async function connectToDatabase() {
-    if (!client.topology || client.topology.isDestroyed()) {
-        await client.connect();
-    }
+  if (!client.topology || client.topology.isDestroyed()) {
+      await client.connect();
+  }
 
-    db = client.db("API");
-    console.log("✅ Connected to MongoDB");
+  db = client.db("API");
+  console.log("✅ Connected to MongoDB");
 }
 
 
@@ -47,56 +47,56 @@ async function connectToDatabase() {
 
 // provjera ako vijest postoji
 async function articleExists(postLink) {
-    const result = await db.collection('vjesti').findOne({ link: postLink });
-    
-    // ako nesto postoji onda vraca true
-    // ako nista ne postoji onda vraca false
-    return !!result;
+  const result = await db.collection('vjesti').findOne({ link: postLink });
+  
+  // ako nesto postoji onda vraca true
+  // ako nista ne postoji onda vraca false
+  return !!result;
 }
 
 
 // spremanje vijest
 async function saveArticle(article) {
-    const result = await db.collection('vjesti').insertOne(article)
+  const result = await db.collection('vjesti').insertOne(article)
 
-    console.log('✅ Saved:', article.title);
+  console.log('✅ Saved:', article.title);
 }
 
 
 
 async function scrapeNewsPage(pageNumber) {
-    try {
-      const BASE_URL_NEWS = 'https://www.fiuman.hr/tag/rijeka/page/';
+  try {
+    const BASE_URL_NEWS = 'https://www.fiuman.hr/tag/rijeka/page/';
 
-      const { data } = await axios.get(`${BASE_URL_NEWS}${pageNumber}`);
-      const $ = cheerio.load(data);
-      const articles = [];
+    const { data } = await axios.get(`${BASE_URL_NEWS}${pageNumber}`);
+    const $ = cheerio.load(data);
+    const articles = [];
 
-      $('.infinite-post').each((i, el) => {
-          const postTitle = $(el).find('h2').text().trim();
-          const postLink = new URL($(el).find('a').attr('href'), BASE_URL_NEWS).href;
-          const postSummary = $(el).find('p').text().trim();
-          const postImg = new URL($(el).find('img').attr('src'), BASE_URL_NEWS).href;
-          articles.push({ title: postTitle, link: postLink, summary: postSummary, imgsrc : postImg });
-      });
+    $('.infinite-post').each((i, el) => {
+      const postTitle = $(el).find('h2').text().trim();
+      const postLink = new URL($(el).find('a').attr('href'), BASE_URL_NEWS).href;
+      const postSummary = $(el).find('p').text().trim();
+      const postImg = new URL($(el).find('img').attr('src'), BASE_URL_NEWS).href;
+      articles.push({ title: postTitle, link: postLink, summary: postSummary, imgsrc : postImg });
+    });
 
-      for (const article of articles) {
-          // Fetch individual post page
-          const { data: postPage } = await axios.get(article.link);
-          const $$ = cheerio.load(postPage);
+    for (const article of articles) {
+      // Fetch individual post page
+      const { data: postPage } = await axios.get(article.link);
+      const $$ = cheerio.load(postPage);
 
-          // Replace with the correct selector for the date
-          const postDate = $$('.post-date').attr('datetime'); // adjust this!
-          article.postDate = postDate;
+      // Replace with the correct selector for the date
+      const postDate = $$('.post-date').attr('datetime'); // adjust this!
+      article.postDate = postDate;
 
-          if (!(await articleExists(article.link))) {
-              await saveArticle(article);
-          }
-      
+      if (!(await articleExists(article.link))) {
+        await saveArticle(article);
       }
-    } catch (err) {
-        console.error('Scrape failed:', err);
+    
     }
+  } catch (err) {
+    console.error('Scrape failed:', err);
+  }
 }
 
 
@@ -163,7 +163,7 @@ async function getUrlsFromSitemap(sitemapUrl) {
 }
 
 
-async function geocodeAddress(address) {
+async function geocodeAdrese(address) {
   const apiKey = 'AIzaSyAOE3XcToyDYmQIZdNyP66YAb5J5BGNafw';
   const encodedAddress = encodeURIComponent(address);
 
@@ -201,9 +201,8 @@ async function povlacenjeAdrese(urlLokacije) {
 }
 
 
-function extractISODate(dateStr) {
+function formatiranjeDatumaPocetka(dateStr) {
 
-  // Match all date formats like 30. 5. or 30.5.
   const dateMatch = dateStr.match(/(\d{1,2})\.\s*(\d{1,2})\.?/);
 
   if (dateMatch.length === 0) return null;
@@ -211,43 +210,78 @@ function extractISODate(dateStr) {
   const [day, month] = dateMatch.slice(1).map(Number);
 
 
+
   const yearMatch = dateStr.match(/\d{1,2}\.\s*\d{1,2}\.\s*(\d{1,4})\./);
 
   const year = yearMatch ? yearMatch.slice(1).map(Number) : new Date().getFullYear();
 
 
-  // Optional time
-  const timeMatch = dateStr.match(/(\d{1,2}):(\d{2})/);
 
-  console.log(timeMatch);
+  const timeMatch = dateStr.match(/(\d{1,2}):(\d{2})/);
 
   const [hour, minute] = timeMatch ? timeMatch.slice(1).map(Number) : [0, 0];
 
-  //return new Date(year, month - 1, day, hour, minute);
-
-  console.log(new Date(year, month - 1, day, hour + 2, minute))
+  return new Date(year, month - 1, day, hour + 2, minute);
 }
 
 
-async function scrapePage(url) {
+function formatiranjeDatumaZavrsetka(dateStr) {
+
+  const dateMatch = dateStr.match(/(\d{1,2})\.\s*(\d{1,2})\.?\s*(?:\d{2,4}\.?\s*)?(?:\|\s*\d{1,2}\:\d{2}\s*)?(?:\-\s*\d{1,2}\:\d{2}\s*)?$/);
+
+  if (dateMatch.length === 0) return null;
+
+  const [day, month] = dateMatch.slice(1).map(Number);
+
+
+
+  const yearMatch = dateStr.match(/\d{1,2}\.\s*\d{1,2}\.?\s*(\d{2,4})\.?\s*(?:\|\s*\d{1,2}\:\d{2}\s*)?(?:\-\s*\d{1,2}\:\d{2}\s*)?$/);
+
+  const year = yearMatch ? yearMatch.slice(1).map(Number) : new Date().getFullYear();
+
+
+
+  const timeMatch = dateStr.match(/(\d{1,2})\:(\d{2})$/);
+
+  const [hour, minute] = timeMatch ? timeMatch.slice(1).map(Number) : [23, 59];
+
+  return new Date(year, month - 1, day, hour + 2, minute);
+}
+
+
+async function scrapeEventPage(url) {
   const res = await axios.get(url);
 
   const $ = cheerio.load(res.data);
+
+  const test = cheerio.load(res.status);
+
+  console.log("Ovo je status: "+test);
+  if (test == 404) {
+    return null
+  }
 
   const eventTitle = $('.fusion-title-2').text().trim();
   const eventTekst = $('meta[name="description"]').attr('content');
   const eventImg = $('meta[property="og:image"]').attr('content');
 
-  const eventDate = $('.fusion-text-1').text().trim();
+  const fullDate = $('.fusion-text-1').text().trim();
 
   const hrefCheck = $('.fusion-text-2').find('a').attr('href');
+
+  const sortDate = formatiranjeDatumaPocetka(fullDate);
+
+  const endDate = formatiranjeDatumaZavrsetka(fullDate);
+
   if (!hrefCheck) {
     return {
       eventTitle,
       eventTekst,
       link: url,
       eventImg,
-      eventDate,
+      fullDate,
+      sortDate,
+      endDate,
       eventLokacija:"NaN"
     };
   }
@@ -258,14 +292,16 @@ async function scrapePage(url) {
 
   const adresa = await povlacenjeAdrese(lokacijaURL.href);
 
-  const koordinate = await geocodeAddress(adresa);
+  const koordinate = await geocodeAdrese(adresa);
 
   return {
     eventTitle,
     eventTekst,
     link: url,
     eventImg,
-    eventDate,
+    fullDate,
+    sortDate,
+    endDate,
     eventLokacija: {
       lokacijaNaziv,
       lokacijaURL: lokacijaURL.href,
@@ -273,17 +309,16 @@ async function scrapePage(url) {
       lat: koordinate?.lat ?? null
     }
   };
-  
-  
+
 }
 
 
-async function runScraper() {
+async function eventScraper() {
 
 
   const sitemapURLovi = await getEventXMLsFromSitemap();
 
-  for (let i = 0; i < 2; i++) {
+  for (let i = 0; i < 1; i++) {
     const urls = await getUrlsFromSitemap(sitemapURLovi[i]);
 
     for (const url of urls) {
@@ -291,8 +326,12 @@ async function runScraper() {
       const exists = await db.collection('dogadaji').findOne({ link: url });
 
       if (!exists) {
-        const data = await scrapePage(url);
+        const data = await scrapeEventPage(url);
 
+        if (data === Null) {
+          console.log("Link skipped!");
+          continue;
+        }
         await db.collection('dogadaji').insertOne(data);
         console.log(`Saved: ${data.eventTitle}`);
 
@@ -305,6 +344,27 @@ async function runScraper() {
 }
 
 
+async function dodavanjeDatumaUBazu() {
+  const dogadaji = await db.collection('dogadaji').find({}).toArray();
+
+
+  for (let i = 0; i < dogadaji.length; i++) {
+    console.log(dogadaji[i].eventTitle);
+
+    
+    const sortDate = formatiranjeDatumaPocetka(dogadaji[i].fullDate);
+    const endDate = formatiranjeDatumaZavrsetka(dogadaji[i].fullDate);
+
+    console.log(`Dodan datum pocetka: ${sortDate}\n`);
+    console.log(`Dodan datum zavrsetka: ${endDate}\n`);
+    
+
+    await db.collection("dogadaji").updateOne({link: dogadaji[i].link}, {$set: {sortDate, endDate}});
+  }
+
+  
+}
+
 
 /*
 █▀▀ █▀█ █▀█ █▄ █ ▀█▀ █▀▀ █▄ █ █▀▄
@@ -312,105 +372,115 @@ async function runScraper() {
 */
 
 app.get('/api/vjesti', async (req, res) => {
-    try {
-        const page = parseInt(req.query.page) || 1;
-        const limit = parseInt(req.query.limit) || 20;
-        const offset = (page - 1) * limit;
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 20;
+    const offset = (page - 1) * limit;
 
-        const collection = db.collection('vjesti');
+    const collection = db.collection('vjesti');
 
-        const data = await collection
-            .find({})
-            .sort({ postDate: -1 }) // Sort by most recent
-            .skip(offset)
-            .limit(limit)
-            .toArray();
-        
-        const totalRows = await collection.countDocuments();
-        const totalPages = Math.ceil(totalRows / limit);
+    const data = await collection
+      .find({})
+      .sort({ postDate: -1 }) // Sort by most recent
+      .skip(offset)
+      .limit(limit)
+      .toArray();
+    
+    const totalRows = await collection.countDocuments();
+    const totalPages = Math.ceil(totalRows / limit);
 
-        res.json({
-            page,
-            limit,
-            totalRows,
-            totalPages,
-            data
-        });
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Greška u serveru' });
-    }
+    res.json({
+      page,
+      limit,
+      totalRows,
+      totalPages,
+      data
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Greška u serveru' });
+  }
 });
 
 
 
 app.get('/api/dogadaji', async (req, res) => {
-    try {
-        const page = parseInt(req.query.page) || 1;
-        const limit = parseInt(req.query.limit) || 20;
-        const offset = (page - 1) * limit;
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 20;
+    const offset = (page - 1) * limit;
 
-        const collection = db.collection('dogadaji');
+    const collection = db.collection('dogadaji');
 
-        const data = await collection
-            .find({})
-            .sort((a, b) => {
+    const data = await collection
+      .find({})
+      .sort({sortDate:-1})
+      .skip(offset)
+      .limit(limit)
+      .toArray();
+    
+    const totalRows = await collection.countDocuments();
+    const totalPages = Math.ceil(totalRows / limit);
 
-              // Pronalazi u stringu "tribe_events-sitemap(neki broj ili nista).xml"
-              // odvaja broj ako postoji
-              // vraca listu sa stringom i brojem (i jos neke vrijednosti koje nisu bitne za ovo)
-              const aMatch = a.match();
-              const bMatch = b.match();
-
-              // Ako aMatch postoji i postoji broj u listi
-              // uzima broj koji je kao string zapisan i pretvara ga u broj
-              // ako ne sprema 0
-              const aIndex = aMatch && aMatch[1] ? parseInt(aMatch[1]) : 0;
-              const bIndex = bMatch && bMatch[1] ? parseInt(bMatch[1]) : 0;
-
-              // odlucuje redoslijed (padajuci)
-              return bIndex - aIndex;
-            })
-            .skip(offset)
-            .limit(limit)
-            .toArray();
-        
-        const totalRows = await collection.countDocuments();
-        const totalPages = Math.ceil(totalRows / limit);
-
-        res.json({
-            page,
-            limit,
-            totalRows,
-            totalPages,
-            data
-        });
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Greška u serveru' });
-    }
+    res.json({
+      page,
+      limit,
+      totalRows,
+      totalPages,
+      data
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Greška u serveru' });
+  }
 });
+
+
+async function testFunc(url) {
+  const res = await axios.get(url);
+  console.log(res);
+
+  const $ = cheerio.load(res.data);
+  console.log("data: "+$);
+  const eventTitle = $('.fusion-title-2').text().trim();
+  console.log("title: "+eventTitle)
+
+  const test = cheerio.load(res);
+
+  console.log("Ovo je status: "+test.data);
+  if (test == 404) {
+    return null
+  }
+
+
+}
+
 
 
 //
 async function start() {
-    await connectToDatabase();
-    //await scrapeNewsPage(1); // run once at startup  
+  await connectToDatabase();
+  await scrapeNewsPage(1); // run once at startup  
 
-    extractISODate("14. 07. 2027. | 0:30 - 28. 07. | 19:15");
-    // --- Scheduler ---
-    cron.schedule('0 * * * *', async () => {
-        console.log('🕒 Running scheduled scrape...');
-        for (let i = 1; i <= 5; i++) {
-        await scrapeNewsPage(i);
-        }
+  //dodavanjeDatumaUBazu();
 
-        await runScraper();
-    });
+  //await testFunc("https://visitrijeka.hr/event/operacija-kamp-9/");
+  await testFunc("https://visitrijeka.hr/event/26-fiumanka-program-11-6/");
 
-    app.listen(port, () => {
-        console.log(`API radi na http://localhost:${port}`);
-    });
+  //await eventScraper();
+  // --- Scheduler ---
+  cron.schedule('0 * * * *', async () => {
+    console.log('🕒 Running scheduled scrape...');
+    for (let i = 1; i <= 5; i++) {
+    await scrapeNewsPage(i);
+    }
+
+    await eventScraper();
+  });
+
+  app.listen(port, () => {
+    console.log(`API radi na http://localhost:${port}`);
+  });
 }
 
 start();
